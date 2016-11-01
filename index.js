66d22ee0a89c1d393502179862332e39ead88dc5
Albert Reel <waxhoya@gmail.com>
2016-10-31 21:42:31 -0700

    out with the old

--- index.js
+++
@@ -1,7 +0,0 @@
-const server = require('./lib/serverHttp');
-const port = 65000
-
-server.listen(port, err => {
-    if (err) console.log('Error: ', err)
-    else console.log('Server is listening on port: ', port);
-});
